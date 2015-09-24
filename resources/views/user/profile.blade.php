231b211b5d080dc9290dd4cf8ae627f80b35962b
vlazar- <vlazar@outlook.com>
2015-09-24 02:29:27 +0200

    User profile fix

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -14,23 +14,23 @@
 
     <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="fonts/css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="../fonts/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../css/animate.min.css" rel="stylesheet">
 
     <!-- Custom styling plus plugins -->
-    <link href="css/custom.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/maps/jquery-jvectormap-2.0.1.css" />
-    <link href="css/icheck/flat/green.css" rel="stylesheet" />
-    <link href="css/floatexamples.css" rel="stylesheet" type="text/css" />
+    <link href="../css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/maps/jquery-jvectormap-2.0.1.css" />
+    <link href="../css/icheck/flat/green.css" rel="stylesheet" />
+    <link href="../css/floatexamples.css" rel="stylesheet" type="text/css" />
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/nprogress.js"></script>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/nprogress.js"></script>
     <script>
         NProgress.start();
     </script>
 
     <!--[if lt IE 9]>
-    <script src="../assets/js/ie8-responsive-file-warning.js"></script>
+    <script src="../../assets/js/ie8-responsive-file-warning.js"></script>
     <![endif]-->
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -82,9 +82,9 @@
     <div id="notif-group" class="tabbed_notifications"></div>
 </div>
 
-<script src="js/bootstrap.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
 
-<script src="js/custom.js"></script>
+<script src="../js/custom.js"></script>
 
 </body>
 
